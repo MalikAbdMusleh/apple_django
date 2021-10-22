5c3a8fba5d43ec07d5a6bf0b5bee3f0021d38047
MalikAbdMusleh <malikabdmusleh@gmail.com>
2021-10-22 23:00:22 +0300

    list add by bootstraps and the model itterated to it

--- apples_types/views.py
+++ apples_types/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Apples
 # Create your views here.
 def types(request):
-    return render(request,"apple_photos.html")
+    get_all_data=Apples.objects.all()
+    data={
+    "get_all_data":get_all_data
+    }
+    return render(request,"apple_photos.html",data)
